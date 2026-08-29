a93e220cb46d10b3ddb24584a4fb1521e68f6b50
Christopher Bruns <cmbruns@rotatingpenguin.com>
2026-08-29 09:35:32 -0700

    Merge MinecraftMapImagePlugin.py into plugin.py. Rearrange folder structure.

---
+++ tests/pillow_minecraft_map_synopsis.py
@@ -0,0 +1,9 @@
+from PIL import Image
+import pillow_minecraft_map  # "unused" import loads the plugin as a side effect
+
+with Image.open("../tests/data/map_111.dat") as img:
+    assert img.mode == "P"
+    for tag in ["x_center", "z_center", "scale", "data_version"]:
+        val = img.info.get(tag)
+        print(f"{tag} = {val}")
+    img.show()
