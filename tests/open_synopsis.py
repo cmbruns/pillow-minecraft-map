67211603da3ac0bcafe17b4dd87d159a0ec188d5
Christopher Bruns <cmbruns@rotatingpenguin.com>
2026-08-31 15:11:25 -0700

    Remove debugging PIL version print.

--- tests/open_synopsis.py
+++ tests/open_synopsis.py
@@ -1,9 +1,7 @@
-import PIL
 from PIL import Image
 import pillow_minecraft_map  # "unused" import loads the plugin as a side effect
 
 with Image.open("../tests/data/map_111.dat") as img:
-    print(PIL.__version__)
     assert img.mode == "P"
     for tag in ["x_center", "z_center", "scale", "data_version"]:
         val = img.info.get(tag)
